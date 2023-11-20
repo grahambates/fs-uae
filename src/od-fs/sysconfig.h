b508a550cad9bddef49dc2359d88bab7ac013e2f
Graham Bates <info@grahambates.com>
2023-11-20 01:52:46 +0000

    3300b3

--- src/od-fs/sysconfig.h
+++ src/od-fs/sysconfig.h
@@ -276,6 +276,8 @@
 /* Define to 1 if you need to in order for `stat' and other things to work. */
 /* #undef _POSIX_SOURCE */
 
+typedef long uae_atomic;
+
 #include "defines.h"
 
 #endif /* FSUAE_SYSCONFIG_H */
